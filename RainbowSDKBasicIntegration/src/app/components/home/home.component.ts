84a66a3e86b27c46428b15df76740274fb411ee9
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-09-03 13:50:14 +0200

    feat: make call audio and video

--- RainbowSDKBasicIntegration/src/app/components/home/home.component.ts
+++ RainbowSDKBasicIntegration/src/app/components/home/home.component.ts
@@ -1,18 +1,19 @@
-import { Component, OnInit, ViewChild, viewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ConnectionService } from '../../services/connection.service';
 import { Message, User } from 'rainbow-web-sdk';
 import { NetworkService } from '../../services/network.service';
 import { CommonModule } from '@angular/common';
 import { ConvService } from '../../services/conv.service';
-import { FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { CallingService } from '../../services/calling.service';
 
 @Component({
   selector: 'app-home',
   imports: [CommonModule, FormsModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent implements OnInit {
   @ViewChild('messagesContainer') messagesContainer: any;
 
   protected connectedUser: User = {} as User;
@@ -22,29 +23,38 @@ export class HomeComponent implements OnInit{
   protected newMessage: string = '';
   protected selectedUser: User = {} as User;
 
-  constructor(private connectionService: ConnectionService, private networkService: NetworkService, private convService: ConvService) {}
+  constructor(
+    private connectionService: ConnectionService,
+    private networkService: NetworkService,
+    private convService: ConvService,
+    protected callingService: CallingService
+  ) {}
 
   ngOnInit(): void {
     const connectionInfo = this.connectionService.isUserConnected();
     this.isConnected = connectionInfo.connected;
-
   }
 
   protected connect() {
-    this.connectionService.init().then(() => {
-      const connectionInfo = this.connectionService.isUserConnected();
-      this.isConnected = connectionInfo.connected;
-      this.connectedUser = connectionInfo.user ? connectionInfo.user : {} as User;
-      this.getContacts();
-    }).catch((error) => {
-      console.error(`[testAppli] ${error.message}`);
-    });
+    this.connectionService
+      .init()
+      .then(() => {
+        const connectionInfo = this.connectionService.isUserConnected();
+        this.isConnected = connectionInfo.connected;
+        this.connectedUser = connectionInfo.user
+          ? connectionInfo.user
+          : ({} as User);
+        this.getContacts();
+      })
+      .catch((error) => {
+        console.error(`[testAppli] ${error.message}`);
+      });
   }
 
   scrollToBottom(): void {
     try {
       if (this.messagesContainer) {
-        this.messagesContainer.nativeElement.scrollTop = 
+        this.messagesContainer.nativeElement.scrollTop =
           this.messagesContainer.nativeElement.scrollHeight;
       }
     } catch (err) {
@@ -57,7 +67,7 @@ export class HomeComponent implements OnInit{
   }
 
   protected getMessages(contact: User): void {
-    console.log("Contacts", this.contacts);
+    console.log('Contacts', this.contacts);
     this.selectedUser = contact;
     if (this.contacts && this.contacts.length > 0) {
       this.convService.getMessages(contact).then((messages) => {
--- RainbowSDKBasicIntegration/src/app/services/calling.service.ts
+++ RainbowSDKBasicIntegration/src/app/services/calling.service.ts
@@ -5,24 +5,26 @@ import { CallService, MediaType, RainbowSDK, User } from 'rainbow-web-sdk';
   providedIn: 'root'
 })
 export class CallingService {
-  private rainbowSDK: RainbowSDK;
   private medias: MediaType[] = [MediaType.AUDIO, MediaType.VIDEO];
-  callService: CallService = {} as CallService;
 
   constructor() { 
-    this.rainbowSDK = RainbowSDK.getInstance();
-    this.callService = this.rainbowSDK.callService ? this.rainbowSDK.callService : {} as CallService;
   }
 
-  getCallService(): CallService {
-    return this.callService;
+  async getCallService(): Promise<CallService> {
+    const rainbowSDK = RainbowSDK.getInstance();
+    const callService = rainbowSDK.callService ? rainbowSDK.callService : {} as CallService;
+    return callService;
   }
 
   makeAudioCall(user: User): void {
-    this.callService.makeWebCall(user)
+    this.getCallService().then(callService => {
+      callService.makeWebCall(user);
+    });
   }
 
   makeVideoCall(user: User): void {
-    this.callService.makeWebCall(user, this.medias, "Making some tests")
+    this.getCallService().then(callService => {
+      callService.makeWebCall(user, this.medias, "Making some tests");
+    });
   }
 }

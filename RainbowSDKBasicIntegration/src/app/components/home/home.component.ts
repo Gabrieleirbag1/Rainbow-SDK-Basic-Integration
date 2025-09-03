aa3c045bb6c204d39638200186e41939cd3c9e1a
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-09-03 14:21:50 +0200

    feat: add callevent subscribe

--- RainbowSDKBasicIntegration/src/app/components/home/home.component.ts
+++ RainbowSDKBasicIntegration/src/app/components/home/home.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ConnectionService } from '../../services/connection.service';
-import { Message, User } from 'rainbow-web-sdk';
+import {
+  CallEvents,
+  Conversation,
+  Call,
+  ConversationServiceEvents,
+  Message,
+  RainbowSDK,
+  RBEvent,
+  User,
+} from 'rainbow-web-sdk';
 import { NetworkService } from '../../services/network.service';
 import { CommonModule } from '@angular/common';
 import { ConvService } from '../../services/conv.service';
 import { FormsModule } from '@angular/forms';
 import { CallingService } from '../../services/calling.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -23,16 +33,106 @@ export class HomeComponent implements OnInit {
   protected newMessage: string = '';
   protected selectedUser: User = {} as User;
 
+  private rainbowSDK: RainbowSDK;
+
   constructor(
     private connectionService: ConnectionService,
     private networkService: NetworkService,
     private convService: ConvService,
     protected callingService: CallingService
-  ) {}
+  ) {
+    this.rainbowSDK = RainbowSDK.getInstance();
+    this.subscribeConvServiceEvent();
+  }
 
   ngOnInit(): void {
     const connectionInfo = this.connectionService.isUserConnected();
     this.isConnected = connectionInfo.connected;
+  }
+
+  subscribeConvServiceEvent(): void {
+    const conversationCallSubscription =
+      this.rainbowSDK.conversationService?.subscribe(
+        (event: RBEvent<ConversationServiceEvents>) => {
+          try {
+            const conversation: Conversation = event.data.conversation;
+
+            switch (event.name) {
+              case ConversationServiceEvents.ON_NEW_CALL_IN_CONVERSATION:
+                this.onCallConversationCreated(conversation);
+                break;
+
+              case ConversationServiceEvents.ON_REMOVE_CALL_IN_CONVERSATION:
+                this.onCallConversationRemoved(conversation);
+                break;
+
+              default:
+                break;
+            }
+          } catch (error) {
+            console.error('Error in conversation event handler:', error);
+          }
+        },
+        [
+          ConversationServiceEvents.ON_NEW_CALL_IN_CONVERSATION,
+          ConversationServiceEvents.ON_REMOVE_CALL_IN_CONVERSATION,
+        ]
+      );
+  }
+
+  protected onCallConversationCreated(conversation: Conversation): void {
+    console.log('Call conversation created:', conversation);
+    this.subscribeCallEvent();
+    return;
+  }
+
+  protected onCallConversationRemoved(conversation: Conversation): void {
+    console.log('Call conversation removed:', conversation);
+    return;
+  }
+
+  subscribeCallEvent(): void {
+    const callConversations: Conversation[] =
+      this.rainbowSDK.conversationService?.getCallConversations();
+    console.log('Found call conversations:', callConversations);
+
+    if (callConversations) {
+      // For each call found, subscribe to the events
+      callConversations.forEach(async (conv: any) => {
+        if (conv.call) {
+          this.addCallEventsListeners(conv.call);
+        }
+      });
+    } else {
+      console.error('No call conversations found');
+    }
+  }
+
+  private addCallEventsListeners(call: Call): void {
+    let callSubscription: Subscription = call.subscribe(
+      (event: RBEvent<CallEvents>) => {
+        switch (event.name) {
+          case CallEvents.ON_CALL_STATUS_CHANGE:
+            console.log('Call status changed');
+            break;
+          case CallEvents.ON_CALL_CAPABILITIES_UPDATED:
+            console.log('Call capabilities updated');
+            break;
+          case CallEvents.ON_CALL_MEDIA_UPDATED:
+            console.log('Call media updated');
+            break;
+          case CallEvents.ON_CALL_MUTE_CHANGE:
+            console.log('Call mute changed');
+            break;
+          case CallEvents.ON_CALL_NETWORK_QUALITY_CHANGE:
+            console.info(`Network quality: ${event.data}`);
+            break;
+          default:
+            console.log('Other call event:', event.name);
+            break;
+        }
+      }
+    );
   }
 
   protected connect() {
